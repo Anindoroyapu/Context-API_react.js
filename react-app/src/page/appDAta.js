db1ced0efe56829cb98e21f20e00d433224fce38
Anindo Roy <anindoroy112@gmail.com>
2024-03-07 09:43:06 +0600

    index_file-done

---
+++ react-app/src/page/appDAta.js
@@ -0,0 +1,12 @@
+export const rowData = [
+    {
+        id:1,
+        title:'Car',
+        price:100,
+        companya:"Toyota",
+        info:"One of the best option",
+        inCart:false,
+        count:1,
+
+    }
+]
