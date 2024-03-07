5f9661a61fb4e4fb17ba9e8f7566e559bd145f3e
Anindo Roy <anindoroy112@gmail.com>
2024-03-07 11:48:24 +0600

    app-file-done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
+import React, {createContext, useState} from 'react';
 import './App.css';
-import Home from './Compotent/Home'
+import Images from './Compotent/Images';
+import Jumbutron from './Compotent/Jumbutron';
+import SearchField from './Compotent/SearchField';
+
+
+
 
 function App() {
+  const [searchImage, setSearchImage] = useState('');
+  const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cats&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+    searchImage,
+    setSearchImage
+  }
+
   return (
-    <div className="App">
-      <Home />
-      
-      
-    </div>
+    <ImageContext.Provider value={value}>
+      <Jumbutron>
+        <SearchField />
+      </Jumbutron>
+      <Images />
+    </ImageContext.Provider>
   );
 }
-
 export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {ProductProvider} from'./page/Context'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ProductProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </ProductProvider>
-
   
 );
-
-
-reportWebVitals();
