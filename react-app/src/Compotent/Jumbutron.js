d498e43286b7fe2e19b906ecd3649ff37d6088bd
Anindo Roy <anindoroy112@gmail.com>
2024-03-07 11:50:29 +0600

    Jumbutron_file_done

---
+++ react-app/src/Compotent/Jumbutron.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Jumbutron = ({children}) =>{
+    return(
+        <div className=" ">
+            <div className=" ">
+                <div className=" ">
+                    Find Images
+
+                </div>
+                {children}
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Jumbutron;
