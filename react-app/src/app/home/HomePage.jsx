dcea4249e2bbe6fde29fb707e95cbecfb9893283
Anindo Roy <anindoroy112@gmail.com>
2024-03-15 11:02:21 +0600

    pagination_done

--- react-app/src/app/home/HomePage.jsx
+++ react-app/src/app/home/HomePage.jsx
@@ -4,39 +4,35 @@ import iopt from "./iopt";
 
 function HomePage() {
   // eslint-disable-next-line no-unused-vars
-  const [imagesList, setImagesList] = useState([]);
+  const [imagesList, setImagesList] = useState({});
   const [seachText, setSearchText] = useState("");
-  const [currentPage] = useState(1);
-  const imgsPerPage = 10;
-  const lastIndex = currentPage * imgsPerPage;
-  const firstIndex = lastIndex - imgsPerPage;
-  //const imgs = imagesList.slice(firstIndex, lastIndex);
-  const npage = Math.ceil(imagesList.length / imgsPerPage);
-  const numbers = [...Array(npage + 1).keys()].slice(1);
-
+  const [currentPage, setCurrentPage] = useState(1);
   const Access_Key = process.env.REACT_APP_ACCESS_KEY;
 
+  const pageNumberArr = [...Array(imagesList?.total_pages).keys()];
   useEffect(() => {
     if (seachText.trim().length) {
       axios
         .get(
-          `https://api.unsplash.com/search/photos?page=1&query=${seachText}&client_id=${Access_Key}`
+          `https://api.unsplash.com/search/photos?page=${currentPage}&query=${seachText}&client_id=${Access_Key}`
         )
         .then(({ data }) => {
-          setImagesList(data?.results || []);
+          setImagesList(data);
         })
         .catch((error) => {
           console.log(error);
         });
     }
-  }, [Access_Key, seachText]);
-  console.log(imagesList);
+  }, [Access_Key, currentPage, seachText]);
 
-  const handleSearch = (e) => {};
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setSearchText(ev?.target[0].value);
+  };
 
   return (
     <div className="container bg-body-secondary vh-100">
-      <form className="form-inline bg-info text-center">
+      <form className="form-inline bg-info text-center" onSubmit={handleSubmit}>
         <div className="form-group mx-sm-3 mb-3">
           <label htmlFor="inputText" className="sr-only">
             <div className="p-3 mb-2 fs-2 fw-bold ">Find Your Images</div>
@@ -47,12 +43,11 @@ function HomePage() {
               className="form-control mb-4 me-3"
               id="inputText"
               placeholder="Search images"
-              onChange={(e) => setSearchText(e.target.value)}
             />
             <button
               type="submit"
               className="btn btn-primary mb-4"
-              onClick={() => handleSearch()}
+              onClick={() => null}
             >
               Search
             </button>
@@ -63,43 +58,57 @@ function HomePage() {
         <nav aria-label="Page navigation example">
           <ul className="pagination justify-content-center">
             <li className="page-item">
-              <a href=" " className="page-link" onClick={prePage}>
-                Previous
-              </a>
-            </li>
-            {numbers.map((n, i) => (
-              <li
-                className={`page-item ${currentPage === n ? "active" : ""}`}
-                key={i}
+              <button
+                onClick={() =>
+                  setCurrentPage((cuData) => {
+                    if (cuData >= 1) {
+                      return cuData - 1;
+                    }
+                    return cuData;
+                  })
+                }
+                className="page-link"
+                disabled={imagesList?.results === 0}
               >
-                <a
-                  className="page-link"
-                  href=" "
-                  onClick={() => changeCpage(n)}
-                >
-                  {n}
-                </a>
-              </li>
-            ))}
+                Previous
+              </button>
+            </li>
+            {pageNumberArr?.length === 1
+              ? null
+              : pageNumberArr?.slice(0, 20)?.map((item) => {
+                  return (
+                    <li
+                      className={`page-link ${
+                        currentPage === item + 1 ? "active" : ""
+                      }`}
+                      key={item}
+                      onClick={() => setCurrentPage(item + 1)}
+                    >
+                      {item + 1}
+                    </li>
+                  );
+                })}
+
             <li className="page-item">
-              <a className="page-link" href=" " onClick={nextPage}>
+              <button
+                className="page-link"
+                onClick={() => setCurrentPage((cuData) => cuData + 1)}
+                disabled={imagesList?.results === 0}
+              >
                 Next
-              </a>
+              </button>
             </li>
           </ul>
         </nav>
         <h3>Images:{seachText || ""}</h3>
         <div className="d-flex w-100 flex-wrap">
-          {imagesList?.map((item) => {
+          {imagesList?.results?.map((item) => {
             return (
-              <div
-                key={item?.id}
-                className=""
-              >
+              <div key={item?.id} className="">
                 <img
-                  src={iopt(item?.urls?.full, 200)}
+                  src={iopt(item?.urls?.full, 300)}
                   alt="..."
-                  className="img-thumbnail flex-wrap"
+                  className="img-thumbnail"
                   style={{ width: "200px", height: "200px" }}
                 />
               </div>
@@ -109,13 +118,6 @@ function HomePage() {
       </div>
     </div>
   );
-  function prePage() {
-    if (currentPage !== firstIndex) {
-      setImagesList(currentPage - 1);
-    }
-  }
-  function changeCpage(id) {}
-  function nextPage() {}
 }
 
 export default HomePage;
